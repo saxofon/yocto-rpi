0d5382c6d59183254b4ccf9a66e10244a482d881
Per Hallsmark <per.hallsmark@windriver.com>
2021-12-02 11:08:07 +0100

    build container: scope in writeable mounts
    Signed-off-by: Per Hallsmark <per.hallsmark@windriver.com>

--- lib.mk/build-container.mk
+++ lib.mk/build-container.mk
@@ -15,13 +15,17 @@ build-container-image:
 	docker build --no-cache -f dockerfiles/yocto-builder -t $(BUILD_CONTAINER_IMAGE) dockerfiles
 
 build-container-start:
-	$(Q)docker run --rm -d --name $(BUILD_CONTAINER) -u builder -v $(shell pwd):$(shell pwd):shared -it $(BUILD_CONTAINER_IMAGE) /bin/bash
+	$(Q)docker run --rm -d --name $(BUILD_CONTAINER) -u builder \
+		-v $(DOWNLOADS_CACHE):/cache/downloads:shared,Z \
+		-v $(SSTATE_MIRROR):/cache/sstate-mirror:shared \
+		-v $(shell pwd):$(shell pwd):shared,Z \
+		-it $(BUILD_CONTAINER_IMAGE) /bin/bash
 
 build-container-shell:
 	$(Q)docker exec -u builder -w $(shell pwd) -e LANG=en_US.UTF-8 -it $(BUILD_CONTAINER) /bin/bash
 
 build-container-root-shell:
-	$(Q)docker exec -u 0:0 -w $(shell pwd) -e LANG=en_US.UTF-8 -it $(BUILD_CONTAINER) /bin/bash
+	$(Q)docker exec -u 0:0 -w /root -e LANG=en_US.UTF-8 -it $(BUILD_CONTAINER) /bin/bash
 
 build-container-exec:
 	$(Q)docker exec -u builder -w $(shell pwd) -e LANG=en_US.UTF-8 -it $(BUILD_CONTAINER) $(CMD)
