0c8cf3b7e086fef7a64b6fbcc642453862a4df41
Per Hallsmark <per.hallsmark@windriver.com>
2022-01-18 09:47:44 +0100

    top Makefile: sdcard fragment
    Signed-off-by: Per Hallsmark <per.hallsmark@windriver.com>

---
+++ lib.mk/sdcard.mk
@@ -0,0 +1,8 @@
+SDCARD_IMAGE ?= $(TOP)/build/build/tmp/deploy/images/$(word 1,$(MACHINES))/$(word 1,$(IMAGES))-$(word 1,$(MACHINES)).rpi-sdimg
+SDCARD_DEV ?= /dev/mmcblk0
+
+sdcard-deploy:
+	$(Q)for m in $$(lsblk -no mountpoints $(SDCARD_DEV)); do \
+		umount $$m ; \
+	done
+	dd if=$(SDCARD_IMAGE) of=$(SDCARD_DEV) status=progress
