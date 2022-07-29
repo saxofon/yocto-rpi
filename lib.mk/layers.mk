5b69de5ebffa20f35bd0c8fd175645de226fd33d
Per Hallsmark <per@hallsmark.se>
2022-07-29 14:23:50 +0200

    fix layer locking after layer refactoring
    Signed-off-by: Per Hallsmark <per@hallsmark.se>

--- lib.mk/layers.mk
+++ lib.mk/layers.mk
@@ -1,13 +1,11 @@
-MAKE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
-
 #BASE=honister
 BASE=hardknott
 
 define layer-rev
-	case $$(grep -w $$(basename $(1) .git) $(MAKE_PATH) | wc -l) in \
+	case $$(grep -w $$(basename $(1) .git) lib.mk/layer-index/*.mk | wc -l) in \
 	  0) echo "# $$(basename $(1) .git) is a local project layer" ;;  \
-	  1) master_layer="$$(grep -w $$(basename $(1) .git) $(MAKE_PATH) | cut -d: -f2)"; \
-	     echo "# $$(basename $(1) .git) is an embedded layer in $$master_layer"; \
+	  1) master_layer="$$(grep -w $$(basename $(1) .git) lib.mk/layer-index/*.mk | cut -d: -f3 | sed s#\$$\(BDIR\)#$(BDIR)#g)"; \
+	     echo "# $$(basename $(1) .git) is an embedded layer in $$(basename $$master_layer)"; \
 	     echo -n "$$(basename $$master_layer .git)_rev := "; \
 	     echo $$(git -C $$master_layer rev-parse HEAD) ;; \
 	  *) echo -n "$$(basename $(1) .git)_rev := "; \
@@ -16,7 +14,8 @@ define layer-rev
 endef
 
 layer-revisions:
-	$(Q)$(foreach LAYER, $(BDIR)/poky $(LAYERS), $(call layer-rev, $(LAYER));)
+	$(Q)echo "poky_rev := $(shell git -C $(BDIR)/poky rev-parse HEAD)"
+	$(Q)$(foreach LAYER, $(LAYERS), $(call layer-rev, $(LAYER));)
 
 update-layer-lock:
 	$(Q)make layer-revisions | grep -v ^make | sort | uniq > $(TOP)/layer-versions.txt
