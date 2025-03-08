3762734dab1da3a173083b9ebbba8b7a932105bd
Per Hallsmark <per.hallsmark@bitjuggler.se>
2025-03-08 12:02:04 +0100

    bugfixes
    Signed-off-by: Per Hallsmark <per.hallsmark@bitjuggler.se>

--- lib.mk/build-container.mk
+++ lib.mk/build-container.mk
@@ -18,7 +18,7 @@ build-container-start:
 	$(Q)docker run --rm -d --name $(BUILD_CONTAINER) -u builder \
 		-v $(PWD):/src:Z \
 		--mount type=volume,src=yocto-$(shell git branch --show-current | tr / -),target=/build \
-		--mount type=volume,src=yocto-cache-downloads,target=$(DOWNLOAD_CACHE) \
+		--mount type=volume,src=yocto-cache-downloads,target=$(DOWNLOADS_CACHE) \
 		--mount type=volume,src=yocto-cache-sstate,target=$(SSTATE_DIR) \
 		-it $(BUILD_CONTAINER_IMAGE) /bin/bash
 
